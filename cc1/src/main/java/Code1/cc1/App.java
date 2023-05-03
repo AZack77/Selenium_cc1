b077e26bfc745aa8cd3d49fb4f28f660d03afd67
Arwin Sekar <100914887+AZack77@users.noreply.github.com>
2023-05-03 16:36:22 +0530

    Add files via upload

---
+++ cc1/src/main/java/Code1/cc1/App.java
@@ -0,0 +1,13 @@
+package Code1.cc1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
